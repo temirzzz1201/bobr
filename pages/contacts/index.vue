45d18779a859b51d37f2fd4311e15fd8ef8b2cb0
temirzzz1201 <tmzzz@inbox.ru>
2025-04-09 18:03:36 +0300

    seo fix

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <section class="bg-white">
+  <section class="max-w-4xl mx-auto bg-white">
     <h1 class="text-4xl font-bold text-center text-gray-800 mb-8">Связаться с нами</h1>
-
       <div class="bg-gray-50 p-6 rounded-lg shadow">
         <h2 class="text-xl font-semibold text-gray-800 mb-8">Наши контакты</h2>
         <p class="text-lg text-gray-700 leading-relaxed mb-16">
@@ -13,7 +12,6 @@
           <p class="mb-4"><strong>Email:</strong> <a href="mailto:info@рабочий-дом.москва" class="text-blue-600 hover:underline">info@mail.ru</a></p>
           <p><strong>Адрес:</strong> Москва, ул. Примерная, д. 10</p>
         </div>
-
         <div>
           <h2 class="text-2xl font-semibold mb-8">Часы работы</h2>
           <p>Пн–Вс: с 9:00 до 20:00</p>
